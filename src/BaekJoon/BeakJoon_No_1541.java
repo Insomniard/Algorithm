96070648697bdde073984c8a3b4f64252f9c30d4
jjeong <psmb2006@naver.com>
2021-09-09 02:27:13 +0900

    greedy

---
+++ src/BaekJoon/BeakJoon_No_1541.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.*;
+public class BeakJoon_No_1541 {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        String minParsearr[] = input.split("\\-");
+        int answer = Integer.parseInt(minParsearr[0]);
+        for(int i=1;i<minParsearr.length;i++){
+            String plParsearr[] = minParsearr[i].split("\\+");
+            int count = plParsearr.length;
+
+            for(String plus : plParsearr){
+                answer -= Integer.parseInt(plus);
+            }
+        }
+        System.out.println(answer);
+    }
+}
