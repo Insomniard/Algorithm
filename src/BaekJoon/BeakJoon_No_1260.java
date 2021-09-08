5c47ebc30d16e276d2344d05a9f0abd25b18084f
jjeong <psmb2006@naver.com>
2021-09-08 23:36:21 +0900

    bfs

--- src/BaekJoon/BeakJoon_No_1260.java
+++ src/BaekJoon/BeakJoon_No_1260.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class BeakJoon_No_1260 {
 
     static boolean[] visit;
-    static int[][] Map;
+    static int[][] graph;
     public static void main(String args[]){
 
         Scanner scan = new Scanner(System.in);
@@ -11,24 +11,41 @@ public class BeakJoon_No_1260 {
         int trunk = scan.nextInt();
         int currentNode = scan.nextInt();
         visit = new boolean[node+1];
-        Map = new int[node+1][node+1];
+        graph = new int[node+1][node+1];
         for(int i=0;i<trunk;i++){
             int x = scan.nextInt();
             int y = scan.nextInt();
-            Map[x][y] = 1;
-            Map[y][x] = 1;
+            graph[x][y] = 1;
+            graph[y][x] = 1;
         }
         dfs(currentNode);
+        visit = new boolean[node+1];
+        System.out.println();
+        bfs(currentNode);
     }
 
-    static void dfs(int currentNode){
+    static void dfs(int currentNode) {
         visit[currentNode] = true;
         System.out.print(currentNode + " ");
-        if(currentNode == Map.length) return;
-        for(int i=1; i<Map.length;i++){
-            if(Map[currentNode][i] == 1 && visit[i] == false) dfs(i);
+        if (currentNode == graph.length) return;
+        for (int i = 1; i < graph.length; i++) {
+            if (graph[currentNode][i] == 1 && visit[i] == false) dfs(i);
         }
-
+    }
+    public static void bfs(int currentNode){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(currentNode);
+        visit[currentNode] = true;
+        while (!q.isEmpty()){
+            currentNode = q.poll();
+            System.out.print(currentNode + " ");
+            for(int i=1;i<graph.length;i++){
+                if(graph[currentNode][i] == 1 && visit[i] == false){
+                    q.add(i);
+                    visit[i] = true;
+                }
+            }
+        }
     }
 
 
