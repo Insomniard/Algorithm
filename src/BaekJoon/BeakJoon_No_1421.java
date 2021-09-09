5d9d90b505036c886a9aa8ba2e875279b4e9b368
jjeong <psmb2006@naver.com>
2021-09-09 19:57:47 +0900

    brute force

--- src/BaekJoon/BeakJoon_No_1421.java
+++ src/BaekJoon/BeakJoon_No_1421.java
@@ -2,27 +2,25 @@ package BaekJoon;
 import java.util.*;
 public class BeakJoon_No_1421 {
     public static void main(String args[]){
-        int answer=0, answer2 = 0;
+        long answer=0, answer2 = 0;
         Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int C = scan.nextInt();
-        int L = scan.nextInt();
-        ArrayList<Integer> tree = new ArrayList<>();
-        for(int i=0;i<N;i++)tree.add(scan.nextInt());
-        System.out.println(tree);
-        for(int i=1;i<= tree.get(N-1);i++){
+        long N = scan.nextLong();
+        long C = scan.nextLong();
+        long L = scan.nextLong();
+        ArrayList<Long> tree = new ArrayList<>();
+        for(long i=0;i<N;i++)tree.add(scan.nextLong());
+        for(long i=1;i<=10000;i++){
             answer2=0;
-            int sum = 0, countTree = 0, temp = 0;
+            long sum = 0, countTree = 0, temp = 0;
             for(int j=0;j<N;j++){
                 countTree = tree.get(j) / i;
                 temp = (tree.get(j) - 1) / i;
                 sum = (i * countTree * L) - (C * temp);
-                if(sum > 0)answer+=sum;
+                if(sum > 0)answer2+=sum;
             }
 
             answer = Math.max(answer, answer2);
         }
         System.out.println(answer);
-
     }
 }
