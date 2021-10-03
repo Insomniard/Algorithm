8f94cebf11ed442181a66080d6996cb27b58540e
jjeong <psmb2006@naver.com>
2021-10-03 20:11:23 +0900

    String

---
+++ src/Programmers/Level_1_NumberAndString.java
@@ -0,0 +1,11 @@
+package Programmers;
+import java.util.*;
+public class Level_1_NumberAndString {
+    public static void main(String args[]){
+        String s = "23four5six7";
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i=0;i<s.length();i++){
+            if(s.contains(arr[i]))s = s.replace(arr[i],Integer.toString(i));
+        }
+    }
+}
