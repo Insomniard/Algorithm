68959bfba581ef3f7d96fb34c12e621122b0c95d
jjeong <psmb2006@naver.com>
2021-10-03 19:26:16 +0900

    String

--- src/Programmers/Level_1_RecommendNewId.java
+++ src/Programmers/Level_1_RecommendNewId.java
@@ -2,7 +2,7 @@ package Programmers;
 import java.util.*;
 public class Level_1_RecommendNewId {
     public static void main(String args[]){
-        ArrayList<Boolean> check = new ArrayList<>();
+        /*
         String new_id = "abcdefghijklmn.p";
         new_id = new_id.toLowerCase();
         String[] newId = new_id.split("");
@@ -34,5 +34,24 @@ public class Level_1_RecommendNewId {
         if (answer.endsWith(".")) {
             answer = answer.substring(0, answer.length() - 1);
         }
+        => 기본적인 방식
+         */
+
+        /*
+        String new_id = "abcdefghijklmn.p";
+        new_id = new_id.toLowerCase();
+        String answer=new_id;
+        answer = answer.replaceAll("[^-_.a-z0-9]", "");
+        answer = answer.replaceAll("[.]{2,}",".");
+        answer = answer.replaceAll("^[.]|[.]$","");
+        if(answer.equals(""))answer+="a";
+        if(answer.length() > 15){
+            answer = answer.substring(0,15);
+            answer = answer.replaceAll("^[.]|[.]$","");
+        }
+        if(answer.length()<3)while (answer.length() < 3)answer+= answer.charAt(answer.length()-1);
+        System.out.println(answer);
+        => 정규식
+        */
     }
 }
