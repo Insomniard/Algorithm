743f0743a9fca6f79a5fb0e74ddbd7987549d643
jjeong <psmb2006@naver.com>
2021-10-11 21:07:59 +0900

    정규식

---
+++ src/LeetCode/RegularExpressionMatching.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+public class RegularExpressionMatching {
+    public static void main(String args[]){
+        String s = "aa";
+        String p = "a";
+        boolean answer = false;
+        if(s.matches(p))answer = true;
+        else answer = false;
+    }
+}
