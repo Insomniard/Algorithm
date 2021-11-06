3f7cb9c1f3c430ce573ebbf3ff51c75c4edd09cd
jjeong <psmb2006@naver.com>
2021-11-06 15:59:58 +0900

    resolve

--- src/BaekJoon/Silver/BeakJoon_No_1010.java
+++ src/BaekJoon/Silver/BeakJoon_No_1010.java
@@ -1,22 +1,28 @@
 package BaekJoon.Silver;
-import java.util.*;
+
+import java.util.Scanner;
+
 public class BeakJoon_No_1010 {
-    static long[][] visit= new long[31][31];
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         //M! / N! * (M-N)!
-        for(int i=0;i<31;i++){
-            visit[i][i] = 1;
-            visit[i][1] = i;
-        }
-        for(int i=2;i<30;i++)
-            for(int j=2;j<30;j++)
-                visit[i][j] = visit[i-1][j-1] + visit[i-1][j];
         int T = scan.nextInt();
         for(int i=0;i<T;i++){
-            int N = scan.nextInt();
-            int M = scan.nextInt();
-            System.out.println(visit[M][N]);
+            double N = scan.nextDouble();
+            double M = scan.nextDouble();
+            System.out.printf("%.0f\n", comb(M,N));
         }
+    }
+
+    private static double comb(double n, double m){
+        double N = 1;
+        double M = 1;
+        for (double i=n;i>n-m;i--){
+            N *= i;
+        }
+        for(double j= m;j>0;j--){
+            M *= j;
+        }
+        return N/M;
     }
 }
