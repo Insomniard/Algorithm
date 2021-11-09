3b5202b6d8609e48d2b5605b2973992a9e7d86b4
jjeong <psmb2006@naver.com>
2021-11-09 19:06:11 +0900

    bfs

---
+++ src/Programmers/Level_2/Level_2_gameMapShortestPath.java
@@ -0,0 +1,62 @@
+package Programmers.Level_2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Level_2_gameMapShortestPath {
+
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) {
+        int[][] maps = {{1, 0, 1, 1, 1},
+                        {1, 0, 1, 0, 1},
+                        {1, 0, 1, 1, 1},
+                        {1, 1, 1, 0, 1},
+                        {0, 0, 0, 0, 1}};
+        Level_2_gameMapShortestPath getAnswer = new Level_2_gameMapShortestPath();
+        System.out.println(getAnswer.solution(maps));
+
+    }
+
+    public int solution(int[][] maps) {
+        int answer = 0;
+        answer = bfs(0, 0, maps);
+        return answer;
+    }
+
+    private static int bfs(int x, int y, int[][] maps) {
+        int answer =1000000;
+        Queue<pos> q = new LinkedList<>();
+        q.add(new pos(x, y,1));
+        int count = 0;
+        while (!q.isEmpty()) {
+            pos current = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int sx = current.x + dx[i];
+                int sy = current.y + dy[i];
+                if (sx >= 0 && sy >= 0 && sx < maps.length && sy < maps[0].length) {
+                    if(maps[sx][sy] == 1){
+                        q.add(new pos(sx,sy, current.distance + 1));
+                    }
+                }
+            }
+
+            System.out.println(answer);
+            if(current.x == maps.length-1 && current.y == maps[0].length -1)return current.distance;
+        }
+        return -1;
+    }
+
+    static class pos {
+        int x;
+        int y;
+        int distance;
+        public pos(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
+    }
+
+}
