b3a24a86804e8119ecaf1ca2ff02bb92f0d42eb9
jjeong <psmb2006@naver.com>
2021-11-13 18:47:02 +0900

    stack

---
+++ src/Programmers/Level_2/Level_2_pariRemove.java
@@ -0,0 +1,28 @@
+package Programmers.Level_2;
+
+import java.util.Stack;
+
+public class Level_2_pariRemove {
+    public static void main(String[] args) {
+        String s = "bbaabb";
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if(stack.isEmpty())stack.push(s.charAt(i));
+            if(stack.peek() == s.charAt(i)){
+                stack.pop();
+                continue;
+            }
+            stack.push(s.charAt(i));
+        }
+        if(stack.isEmpty()){
+            answer = 1;
+        }
+        else{
+            answer = 0;
+        }
+        System.out.println(stack.toString());
+    }
+}
+
