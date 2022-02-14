6d0a979f7132e83988e134d35b0a07bd2c32acd2
bonohbonoh <psmb2006@naver.com>
2022-02-14 23:32:40 +0900

    Math

--- src/BaekJoon/Gold/BeakJoon_No_1323.java
+++ src/BaekJoon/Gold/BeakJoon_No_1323.java
@@ -9,19 +9,24 @@ import java.util.StringTokenizer;
 
 public class BeakJoon_No_1323 {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         long div = Long.parseLong(st.nextToken());
         int divnum = Integer.parseInt(st.nextToken());
+
         if (div % divnum == 0) {
             System.out.println(1);
             return;
         }
+
         long num = div;
         Set<Long> set = new HashSet<>();
         int count = 0;
         long temp = div % divnum;
+
         while (temp != 0) {
+
             temp = div % divnum;
 
             if (!set.add(temp)) {
@@ -30,7 +35,9 @@ public class BeakJoon_No_1323 {
             }
             div = Long.parseLong(Long.toString(temp) + Long.toString(num));
             count++;
+
         }
+
         System.out.println(count);
     }
 
